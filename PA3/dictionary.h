ad1b033769c34e4635d18f6f3e859e4b7faf309b
klittle <kyler.little@wsu.edu>
2017-10-17 19:39:24 -0700

    json file tokenizer

--- PA3/dictionary.h
+++ PA3/dictionary.h
@@ -1,7 +1,7 @@
 
 /*
  *  dictionary.h -- A hash-based dictionary for searching definitions
- *   Copyright 2017 - Aaron S. Crandall <acrandal@gmail.com>
+ *   Copyright 2017 - Kyler Little
  */
 
 #ifndef __DICT_H
@@ -14,12 +14,24 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <cctype>
 
-// LAST UPDATE: 6:03 AM 10/17/17
-// then write a basic function to read input of json file
-// basic file open, (getline() loop), then spit back out to screen
-// then play with that to tokenize it
 
+/*
+LAST UPDATED: 7:37 PM 10/17/17
+I have the json file completely tokenized now. However, I'm not satisified.
+One, I can improve the removeDelimiters function to remove more than one delimiter
+Two, in 'parseline,' I would like to add the entire definition of a word as an entry
+rather than individual words. This will save time in printing even if it makes it less extensible.
+ */
+
+
+/*
+ * UTILITY FUNCTIONS
+ * These functions are not a part of the class definition; however, they are used by the class.
+*/
+
+// str2int
 // SOURCE: https://stackoverflow.com/questions/16388510/evaluate-a-string-with-a-switch-in-c
 // Allows for C++ switch statements using strings
 // Converts string to a constant unsigned int to use in switch statement
@@ -27,38 +39,68 @@ constexpr unsigned int str2int(const char * str, int h = 0) {
     return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
 }
 
+// Parse the line into strings based on the white space delimiter
+// Push_back all tokens into "parsedLine" vector
+void parseline( string line, vector<string> & parsedLine ) {
+  if (!line.empty()) { // not empty
+    string buffer;  // buffer to read token into
+    stringstream ss(line);  // insert line into a stream
+    
+    while (ss >> buffer) {  // capitalizes on the fact that '>>' stops at whitespace
+      if (!buffer.empty())  // if token is actually something
+	parsedLine.push_back(buffer);
+    }
+  } 
+}
+  
+// Utility function to remove a delimiter from a given string
+void removeDelimiters(string & str, char delimiter) {
+  std::size_t pos = str.find_first_of(delimiter, 0);
+  while (pos != string::npos) {
+    str.erase(pos, 1);
+    pos = str.find_first_of(delimiter, 0);
+  }
+}
+
+// Utility function to convert a word to all lower case letters
+void convertToLowerCase(string & word) {
+  int diff = 'A' - 'a';
+  for (auto & letter : word) {
+    if (isupper(letter))
+      letter -= diff;
+  }
+}
+
+
+
+/*
+ * CLASS DEFINITION: Dictionary
+ */
 class Dictionary
 {
  private:
   Hashtable<string, Word> _dict;  // Primary dictionary store
 
-  // Parse the line into strings based on the white space delimiter
-  // Push_back all tokens into "parsedLine" vector
-  void parseline( string line, vector<string> & parsedLine ) {
-    if (!line.empty()) { // not empty
-      string buffer;  // buffer to read token into
-      stringstream ss(line);  // insert line into a stream
-      
-      while (ss >> buffer) {  // capitalizes on the fact that '>>' stops at whitespace
-	if (!buffer.empty())  // if token is actually something
-	  parsedLine.push_back(buffer);
-      }
-    } 
-  }
-
-  // Precondition: parsedLine vector
-  // Utility function called if command is "add," "remove," or "define" to clean up the vector
-  // Converts the "word" to all lower case for lookup & removes any enclosing quotations on
-  // the "word" or "definition"
-  void cleanStrings(vector<string> parsedLine) {
-    // can I use this for json format? One sec...
-  }
-
   void load(string &filename) {
     std::ifstream input;
-    input.exceptions(std::ifstream::failbit | std::ifstream::badbit);  // Allows exception to be thrown if failure opening file
+    input.exceptions(std::ifstream::failbit | std::ifstream::badbit);  // Allows exception to be thrown if failure occurs when opening
     try {
       input.open(filename.c_str());
+      string jsonLine;
+      vector<string> lineVec;
+      while (getline(input, jsonLine)) {
+	parseline(jsonLine, lineVec);
+	for (auto & token : lineVec) {
+	  for (auto & delimiter : {'{','}', '[', ']', '\"', ','})
+	    removeDelimiters(token, delimiter);
+	  if (token == "word:" || token == "definition:" || token == "dictionary:")
+	    token.clear();
+	  cout << token;
+	}
+	cout << endl;
+	lineVec.clear();
+      }
+      input.close();
     }
     catch (std::ios_base::failure & f) {
       cout << "Failed to open JSON file named " << filename << endl;
